c2a2f94531947c729752ce30d83e789fa35eb235
aib0419 <96705261+aib0419@users.noreply.github.com>
2025-05-22 20:00:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,20 @@ import os
 import csv
 import json, re
 
+#Firebase関連
+import firebase_admin
+from firebase_admin import credentials, firestore
+import json
+
+# Firebase Admin SDKの初期化（すでに初期化されていない場合のみ）
+if not firebase_admin._apps:
+    firebase_info = json.loads(st.secrets["firebase"])  # ← secrets.toml の内容を取得
+    cred = credentials.Certificate(firebase_info)
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()  # Firestoreに接続
+
+
 st.title("🧠 Re:Me – 自己内省AI")
 
 # OpenAIキー
